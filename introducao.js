0cfef88c5a43d4bcce2da32a0546f81c3aac2c72
Vinycius <viny.antunes@gmail.com>
2022-02-23 17:13:21 -0300

    introdução á arrays

---
+++ introducao.js
@@ -0,0 +1,18 @@
+// medias 10, 6.5, 5, 8, 7.5
+
+   // let nota1 = 10;
+    //let nota2 = 6.5;
+    //let nota3 = 5;
+    //let nota4 = 8;
+    //let nota5 = 7.5;
+
+    //let media = (nota1 + nota2 + nota3 + nota4 + nota5 )/5
+
+    //console.log(media);
+//--------------------------------
+
+const notas = [10, 6.5, 5, 8, 7.5]
+
+let media = (notas[0] + notas[1] + notas[2] + notas[3] + notas[4])/notas.length;
+
+console.log(media);
